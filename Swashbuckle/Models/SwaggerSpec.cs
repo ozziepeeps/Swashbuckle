c61efc3d9ae7dc3d118a8abcc696096a0693f70e
ozziepeeps <mark@markosborn.me>
2014-01-03 16:58:13 -0800

    Added support for the ResourceListing-level "info" property support by the swagger spec.

--- Swashbuckle/Models/SwaggerSpec.cs
+++ Swashbuckle/Models/SwaggerSpec.cs
@@ -35,6 +35,30 @@ namespace Swashbuckle.Models
 
         [JsonProperty("apis")]
         public IList<ApiDeclarationLink> Apis { get; set; }
+
+        [JsonProperty("info")]
+        public Info Info { get; set; }
+    }
+
+    public class Info
+    {
+        [JsonProperty("contact")]
+        public string Contact { get; set; }
+
+        [JsonProperty("description")]
+        public string Description { get; set; }
+
+        [JsonProperty("license")]
+        public string License { get; set; }
+
+        [JsonProperty("licenseUrl")]
+        public string LicenseUrl { get; set; }
+
+        [JsonProperty("termsOfServiceUrl")]
+        public string TermsOfServiceUrl { get; set; }
+
+        [JsonProperty("title")]
+        public string Title { get; set; }
     }
 
     public class ApiDeclarationLink
